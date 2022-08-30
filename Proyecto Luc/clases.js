9bc1380c1ba7f4ffe4f0041353da587bee96f1dd
FrancoPanfilo <108556532+FrancoPanfilo@users.noreply.github.com>
2022-08-30 19:28:03 -0300

    Paco inicio
    sobre todo agregue un background y un archivo json que servira como base para cargar los renders

---
+++ Proyecto Luc/clases.js
@@ -0,0 +1,13 @@
+class Render {
+    constructor(identificador, estilo) {
+        this.id = identificador,
+            this.imagenes = [],
+            this.estilo = estilo,
+            this.descripcion = ""
+    }
+}
+class LucRenders {
+    constructor() {
+        this.renders = []
+    }
+}
